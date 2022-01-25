d61fe017bc05cb5201b47a0c1b3197239f406f2d
鄧祖恩 <s369236s@gmail.com>
2022-01-25 11:39:06 +0800

    - init

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,14 @@
-import express from "express";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 async function boostrap() {
-    const app = express();
+    const app = (0, express_1.default)();
     const PORT = process.env.PORT || 3000;
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+    app.use(express_1.default.json());
+    app.use(express_1.default.urlencoded({ extended: true }));
     app.get("/", (req, res) => {
         console.log(req.body);
         res.send({ hey: "hey" });

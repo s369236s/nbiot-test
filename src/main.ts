c0c430ecff4d83ee4e4b8b92ef0781d6e6c412b8
鄧祖恩 <s369236s@gmail.com>
2022-01-25 11:50:47 +0800

    remove bo

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
 import express from "express";
 import cors from "cors"; //
 
-async function boostrap() {
-  const app = express();
+const app = express();
 
-  const PORT = process.env.PORT || 3000;
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  app.get("/test", (req, res) => {
-    console.log(req.body);
-    res.send({ hey: "hey" });
-  });
+app.get("/test", (req, res) => {
+  console.log(req.body);
+  res.send({ hey: "hey" });
+});
 
-  app.listen(PORT, () => {
-    console.log(PORT);
-  });
-}
-
-boostrap();
+app.listen(PORT, () => {
+  console.log(PORT);
+});

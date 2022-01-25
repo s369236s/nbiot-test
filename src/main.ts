5a27bce6fa4a8bf79bff768b4606a2370da46c2b
鄧祖恩 <s369236s@gmail.com>
2022-01-25 11:36:14 +0800

    - init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+async function boostrap() {
+  const app = express();
+
+  const PORT = process.env.PORT || 3000;
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get("/", (req, res) => {
+    console.log(req.body);
+    res.send({ hey: "hey" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(PORT);
+  });
+}
+
+boostrap();

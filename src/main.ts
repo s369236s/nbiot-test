c9ff0427ef672f4863a1f3d60b5da4e412b71268
鄧祖恩 <s369236s@gmail.com>
2022-01-25 11:47:14 +0800

    - cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 async function boostrap() {
   const app = express();
 
   const PORT = process.env.PORT || 3000;
-
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
